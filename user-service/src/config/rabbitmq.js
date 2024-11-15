c9087c1bdf5fdfd3be7ad1363c50dde50c1027a0
snir1551 <snir1552@gmail.com>
2024-11-15 19:31:59 +0200

    add connection to mongoDB and rabbitmq

---
+++ user-service/src/config/rabbitmq.js
@@ -0,0 +1,25 @@
+import amqplib from 'amqplib';
+
+export async function connectToRabbitMQ(retries = 5) {
+    let connection;
+    while (retries > 0) {
+      try {
+        connection = await amqplib.connect('amqp://rabbitmq:5672');
+        break;
+      } catch (error) {
+        console.error('Error connecting to RabbitMQ:', error.message);
+        retries -= 1;
+        console.log(`Retrying...${retries} attempts left.`);
+        await new Promise(res => setTimeout(res, 5000)); 
+      }
+    }
+    if (!connection) {
+      console.error('Failed to connect to RabbitMQ after multiple attempts.');
+      process.exit(1);
+    }
+    return connection;
+  }
+
+  
+
+
